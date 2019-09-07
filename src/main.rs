2d21fadcbd533a614f395570c91ae2bda79db864
kabbalah <kabbalah@erirun.net>
2019-09-07 21:23:05 +0900

    change library: wiringpi to rppal

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
-extern crate wiringpi;
+extern crate rppal;
 
-use wiringpi::pin::Value::{High, Low};
+use rppal::gpio::{Gpio, Error};
+use rppal::gpio::Level::{High, Low};
 use std::{thread, time};
 
 static SEGPAT: [u32; 16] = [
@@ -10,82 +11,73 @@ static SEGPAT: [u32; 16] = [
     0x1A, 0x7A, 0x9E, 0x8E  //c d E F
 ];
 
-fn main() {
-    //Setup WiringPi with its own pin numbering order
-    let pi = wiringpi::setup();
+fn main() -> Result<(), Error> {
+    let gpio = Gpio::new()?;
 
-    let digits = [
-        pi.output_pin(6),
-        pi.output_pin(10),
-        pi.output_pin(11),
-        pi.output_pin(27),
-        pi.output_pin(28),
-        pi.output_pin(29)
+    let mut digits = [
+        gpio.get(25)?.into_output(),
+        gpio.get(8)?.into_output(),
+        gpio.get(7)?.into_output(),
+        gpio.get(16)?.into_output(),
+        gpio.get(20)?.into_output(),
+        gpio.get(21)?.into_output(),
     ];
 
-    let segs = [
-	pi.output_pin(23),
-	pi.output_pin(25),
-	pi.output_pin(21),
-	pi.output_pin(13),
-	pi.output_pin(12),
-	pi.output_pin(24),
-	pi.output_pin(22),
-	pi.output_pin(14)
+    let mut segs = [
+        gpio.get(13)?.into_output(),
+        gpio.get(26)?.into_output(),
+        gpio.get(5)?.into_output(),
+        gpio.get(9)?.into_output(),
+        gpio.get(10)?.into_output(),
+        gpio.get(19)?.into_output(),
+        gpio.get(6)?.into_output(),
+        gpio.get(11)?.into_output(),
     ];
 
     let dig_interval = time::Duration::from_millis(1);
     let dsp_interval = time::Duration::from_millis(1);
 
-    let dispseg = |d, s| {
-	for x in 0..6 {
-	    let val = if x == d {
-		High
-	    } else {
-     		Low
-	    };
-	    digits[x as usize].digital_write(val);
+    let mut dispseg = |d, s| {
+        for x in 0..6 {
+            let val = if x == d { High } else { Low };
+            digits[x as usize].write(val);
         }
         for y in 0..8 {
-            let mask = 1 << (7-y);
-	    let seg = if (s & mask) == mask {
-		Low
-	    } else {
-		High
-	    };
-	    segs[y as usize].digital_write(seg);
-	}
-	true
+            let mask = 1 << (7 - y);
+            let seg = if (s & mask) == mask { Low } else { High };
+            segs[y as usize].write(seg);
+        }
+        true
     };
 
     let mut data = [0, 0, 0, 0, 0, 0];
 
     loop {
-	for i in 0..16 {
+        for i in 0..16 {
             for s in 0..6 {
-		data[s as usize] = (i + s) % 16;
-	    }
-	    let mut otime = pi.millis();
-	    let mut ptime = otime;
-	    let mut dp = 0;
-	    loop {
-		let time = pi.millis();
-                if time >= ptime + 500 {
-		    dp = 1 - dp;
-		    ptime = time;
-		}
-	        for d in 0..6 {
-		    let ddp = if d % 2 == 0 { dp } else { 1 - dp };
-	            dispseg(d, SEGPAT[data[d as usize] as usize] | ddp);
+                data[s as usize] = (i + s) % 16;
+            }
+            let mut otime = time::Instant::now();
+            let mut ptime = otime;
+            let mut dp = 0;
+            loop {
+                let time = time::Instant::now();
+                if time.duration_since(ptime).as_millis() > 500 {
+                    dp = 1 - dp;
+                    ptime = time;
+                }
+                for d in 0..6 {
+                    let ddp = if d % 2 == 0 { dp } else { 1 - dp };
+                    dispseg(d, SEGPAT[data[d as usize] as usize] | ddp);
                     //println!("d: {}, data: {}", d, data[d as usize]);
                     thread::sleep(dig_interval);
-	        }
-		if time >= otime + 1000 {
-		    otime = time;
-		    break;
-		}
-	        thread::sleep(dsp_interval);
-	    }
+                }
+                if time.duration_since(otime).as_millis() > 1000 {
+                    otime = time;
+                    break;
+                }
+                thread::sleep(dsp_interval);
+            }
         }
-    };
+    }
 }
